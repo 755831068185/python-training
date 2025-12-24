6b88c36eada9e115fb6e96d96cc0eb4ec3621c77
755831068185 <reethushaiksr@gmail.com>
2025-12-24 19:57:07 +0530

    Add files via upload

---
+++ Halo  square pattern.py
@@ -0,0 +1,9 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============================= RESTART: C:\Users\REETHU\OneDrive\Documents\New folder\23701A04H0\odd sequence.py =============================
+4
+* * * * 
+*     * 
+*     * 
+* * * * 
